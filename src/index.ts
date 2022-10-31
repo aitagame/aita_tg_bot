a9cd2fb4a18d0434063b16a558befa863e8a030e
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-11-01 00:02:25 +0300

    Restore tasks function removed from entry point
    Now it's in /tools/restoreTasks.ts

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,8 @@ import bot from './config/bot'
 import redis from './config/redis'
 import textController from '@handlers/textController';
 import callbackQueryController from '@handlers/cbQueryController';
-import { userData } from 'types/redisUserData'
-import finishWork from '@tools/finishWork';
+import restoreTasks from '@tools/restoreTasks';
 
-async function restoreTasks() {
-    const keysList = await redis.keys('*')
-    if (!keysList) {
-        return console.log('Empty set.')
-    }
-    keysList.forEach(async (item) => {
-        const response = await redis.get(item) as string
-        const userData = JSON.parse(response) as userData
-
-        if (userData.state.action === 'idle' || userData.state.start === null || userData.state.end === null) {
-            return
-        }
-
-        const elapsedTime = userData.state.end - Date.now()
-
-        setTimeout(() => {
-            finishWork(userData.user_id)
-        }, elapsedTime)
-    })
-}
 
 async function startServer() {
     try {
@@ -43,10 +22,11 @@ async function startServer() {
     }
 }
 
-startServer().then(() => {
-    restoreTasks().then(() => {
+startServer()
+    .then(() => {
+        restoreTasks()
+    })
+    .then(() => {
         bot.on('text', textController)
         bot.on('callback_query', callbackQueryController)
     })
-})
-
---
+++ src/tools/restoreTasks.ts
@@ -0,0 +1,26 @@
+import redis from "@src/config/redis"
+import finishWork from "./finishWork"
+import { userData } from 'types/redisUserData'
+
+async function restoreTasks() {
+    const keysList = await redis.keys('*')
+    if (!keysList) {
+        return console.log('Empty set.')
+    }
+    keysList.forEach(async (item) => {
+        const response = await redis.get(item) as string
+        const userData = JSON.parse(response) as userData
+
+        if (userData.state.action === 'idle' || userData.state.start === null || userData.state.end === null) {
+            return
+        }
+
+        const elapsedTime = userData.state.end - Date.now()
+
+        setTimeout(() => {
+            finishWork(userData.user_id)
+        }, elapsedTime)
+    })
+}
+
+export default restoreTasks
