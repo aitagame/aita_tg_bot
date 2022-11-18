389e2767d792436bbf8d99bcc5f4cb35c6a33759
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-11-18 09:53:57 +0300

    BUG FIX
    Fixed an issue where a newly registered user had no state data

--- src/handlers/commands/registerUser.ts
+++ src/handlers/commands/registerUser.ts
@@ -3,6 +3,8 @@ import bot from "@src/config/bot"
 import { Message, User } from "node-telegram-bot-api"
 import Characters from "@sql/charactersDB"
 import getInfo from "./getInfo"
+import redis from "@src/config/redis"
+import { UserDataType } from "@src/types/redisUserData"
 
 async function registerUser(msg: Message) {
     const dbCharacters = new Characters()
@@ -17,9 +19,22 @@ async function registerUser(msg: Message) {
             element: getRandomInt(0, 2),
             name: first_name
         })
+
+        const defaultUserData: UserDataType = {
+            state: {
+                action: 'idle',
+                end: null,
+                start: null
+            },
+            user_id: user_id,
+            chat_id: msg.chat.id
+        }
+
+        await redis.set(user_id.toString(), JSON.stringify(defaultUserData))
+
         return bot.sendMessage(chat_id, 'User created')
     } else return getInfo(msg)
-    
+
 }
 
 export default registerUser
