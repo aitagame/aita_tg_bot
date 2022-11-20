b157884030eea39b4d98732de8ef991428f2da75
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-11-20 20:03:06 +0300

    🛠 Reworked experience system❗️
    📈Added coefficient for triangular formula 📊

--- src/tools/levels.ts
+++ src/tools/levels.ts
@@ -1,8 +1,9 @@
 export function levelExperience(level: number) {
-    return (level / 2) * (level + 1)
+    return ( (level / 2) * (level + 1) ) * 3
 }
 
 export function getLevel(exp: number) {
+    exp /= 3
     return Math.floor(((-2) + Math.sqrt(4 + (32 * exp))) / 4)
 }
 
