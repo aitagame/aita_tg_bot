f5bdf051a8d4879105b612662e4c7401add86c7c
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-11-21 09:32:54 +0300

    ⛔️Bug fix⛔️
    Fixed issues when state of acion does not changes after duel time expire

--- src/tools/duels/deleteDuelRequest.ts
+++ src/tools/duels/deleteDuelRequest.ts
@@ -5,15 +5,19 @@ import { Message } from 'node-telegram-bot-api'
 import createMention from '../createMention'
 
 async function deleteDuelRequest(msg: Message) {
-    const user_id = msg.from?.id as number
-    const user = new UserDataController(user_id)
-    const userData = await user.get()
+    const duelist_user_id = msg.from?.id as number
+    const oponent_user_id = msg.reply_to_message?.from?.id as number
+    const duelist = new UserDataController(duelist_user_id)
+    const oponent = new UserDataController(oponent_user_id)
+    const deulistUserData = await duelist.get()
+    const oponentUserData = await oponent.get()
 
-    if (userData.state.action !== 'duel_pending') return
 
-    const modifiedData: UserDataType = {
-        chat_id: userData.chat_id,
-        user_id: userData.user_id,
+    if (deulistUserData.state.action !== 'duel_pending' || oponentUserData.state.action !== 'duel_pending') return
+
+    const duelist_modifiedData: UserDataType = {
+        chat_id: deulistUserData.chat_id,
+        user_id: deulistUserData.user_id,
         state: {
             action: 'idle',
             end: null,
@@ -21,8 +25,20 @@ async function deleteDuelRequest(msg: Message) {
         }
     }
 
-    user.update(modifiedData)
-    bot.sendMessage(userData.chat_id, `${createMention(msg.from?.first_name as string, user_id)}, The response time for the duel has expired.`, {
+    const oponent_modifiedData: UserDataType = {
+        chat_id: oponentUserData.chat_id,
+        user_id: oponentUserData.user_id,
+        state: {
+            action: 'idle',
+            end: null,
+            start: null
+        }
+    };
+
+    duelist.update(duelist_modifiedData)
+    oponent.update(oponent_modifiedData)
+    bot.deleteMessage(msg.chat.id, msg.message_id.toString())
+    bot.sendMessage(deulistUserData.chat_id, `${createMention(msg.from?.first_name as string, duelist_user_id)}, The response time for the duel has expired.`, {
         parse_mode: 'Markdown'
     })
 }
